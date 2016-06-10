8325147e6441cf3b7982ed719e979be71bd7c870
devin.riley@getbraintree.com <code@getbraintree.com>
2016-06-10 15:43:35 -0700

    Add test for 4_5 (deduping)

--- ch4/4_5/dedupe_test.go
+++ ch4/4_5/dedupe_test.go
@@ -1,14 +1,94 @@
 package dedupe
 
 import(
-  "gopl/ch4/4_5"
   "testing"
 )
 
 func TestNoDupes(t *testing.T) {
   s := []string{"A", "B", "C"}
-  res = Dedupe(s)
-  if res != s {
-    t.Fail()
+  res := Dedupe(s)
+  for i, _ := range res {
+    if res[i] != s[i] {
+      t.Fail()
+    }
   }
 }
+
+func TestOneDupe(t *testing.T) {
+  s := []string{"A", "B", "B", "C"}
+  expected := []string{"A", "B", "C"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
+func TestAdjacentDupes(t *testing.T) {
+  s := []string{"A", "B", "B", "B", "C"}
+  expected := []string{"A", "B", "C"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
+func TestDupesAtBeginning(t *testing.T) {
+  s := []string{"A", "A", "B", "C"}
+  expected := []string{"A", "B", "C"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
+func TestDupesAtEnd(t *testing.T) {
+  s := []string{"A", "B", "C", "C"}
+  expected := []string{"A", "B", "C"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
+func TestMultipleDupes(t *testing.T) {
+  s := []string{"A", "A", "B", "B", "C", "C"}
+  expected := []string{"A", "B", "C"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
+func TestAllDupes(t *testing.T) {
+  s := []string{"A", "A", "A"}
+  expected := []string{"A"}
+
+  res := Dedupe(s)
+
+  for i, _ := range res {
+    if res[i] != expected[i] {
+      t.Fail()
+    }
+  }
+}
+
